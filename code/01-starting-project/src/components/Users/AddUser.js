db452fb222764f27ebe961d60e6a8cf53a71b729
rudgal <office@galler.digital>
2022-07-22 12:25:54 +0200

    Section 8: Practice, 98. Managing the Error State

--- code/01-starting-project/src/components/Users/AddUser.js
+++ code/01-starting-project/src/components/Users/AddUser.js
@@ -7,13 +7,16 @@ import ErrorModal from "../UI/ErrorModal";
 const AddUser = props => {
     const [enteredUsername, setEnteredUsername] = useState('');
     const [enteredAge, setEnteredAge] = useState('');
+    const [error, setError] = useState();
 
     const addUserHandler = (event) => {
         event.preventDefault();
         if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+            setError({title: 'Invalid Input', message: 'Please enter a valid name and age (non-empty values).'});
             return;
         }
         if(+enteredAge < 1) {
+            setError({title: 'Invalid Age', message: 'Please enter a valid age (> 0).'});
             return;
         }
         console.log(enteredUsername, enteredAge)
@@ -31,9 +34,13 @@ const AddUser = props => {
         setEnteredAge(event.target.value)
     };
 
+    const errorHandler = () => {
+        setError(null);
+    }
+
     return (
         <div>
-            <ErrorModal title="An error occurred!" message="Something went wrong!" />
+            {error && <ErrorModal title={error.title} message={error.message} onConfirm={errorHandler}/>}
             <Card className={classes.input}>
                 <form onSubmit={addUserHandler}>
                     <label htmlFor="username">Username</label>
