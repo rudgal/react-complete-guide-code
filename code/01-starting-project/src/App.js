067c3db36ad0fd6299992bb42e61d661bfbf5786
rudgal <office@galler.digital>
2022-07-25 16:52:33 +0200

    Section 11: Practice, 135. Adding a "Meals" Component

--- code/01-starting-project/src/App.js
+++ code/01-starting-project/src/App.js
@@ -1,11 +1,14 @@
 import {Fragment} from "react";
 import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
 
 function App() {
   return (
     <Fragment>
-      <h2>Let's get started!</h2>
         <Header></Header>
+        <main>
+            <Meals />
+        </main>
     </Fragment>
   );
 }
