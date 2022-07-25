9d3d8621556f476b12bac152ebfb747646ccba7a
rudgal <office@galler.digital>
2022-07-25 19:59:10 +0200

    Section 11: Practice: fix add item (+ button) in cart

--- code/01-starting-project/src/components/Cart/Cart.js
+++ code/01-starting-project/src/components/Cart/Cart.js
@@ -14,7 +14,7 @@ const Cart = (props) => {
   };
 
   const cartItemAddHandler = (item) => {
-    cartCtx.addItem({ item, amount: 1 });
+    cartCtx.addItem({ ...item, amount: 1 });
   };
 
   const cartItems = (
