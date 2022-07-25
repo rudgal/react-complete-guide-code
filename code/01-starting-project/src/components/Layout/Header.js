d93cda5cf29959336429bd8120b82fdad520ec48
rudgal <office@galler.digital>
2022-07-25 19:47:44 +0200

    Section 11: Practice: fix

--- code/01-starting-project/src/components/Layout/Header.js
+++ code/01-starting-project/src/components/Layout/Header.js
@@ -8,7 +8,7 @@ const Header = (props) => {
     <Fragment>
       <header className={classes.header}>
         <h1>React Meals</h1>
-        <HeaderCartButton onClick={props.onShowCart}>Card</HeaderCartButton>
+        <HeaderCartButton onClick={props.onShowCart} />
       </header>
       <div className={classes["mainImage"]}>
         <img src={mealsImage} alt="Table full of delicious food" />
