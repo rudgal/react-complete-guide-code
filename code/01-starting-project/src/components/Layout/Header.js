0bc2487221b27fab6a9203c179116f8be94f3b2a
rudgal <office@galler.digital>
2022-07-25 16:31:50 +0200

    Section 11: Practice, 134. Adding the "Cart" Button Component

--- code/01-starting-project/src/components/Layout/Header.js
+++ code/01-starting-project/src/components/Layout/Header.js
@@ -1,13 +1,14 @@
 import React, {Fragment} from 'react';
 import mealsImage from '../../assets/meals.jpg'
 import classes from './Header.module.css'
+import HeaderCartButton from "./HeaderCartButton";
 
 const Header = (props) => {
     return (
         <Fragment>
         <header className={classes.header}>
             <h1>React Meals</h1>
-            <button>Card</button>
+            <HeaderCartButton>Card</HeaderCartButton>
         </header>
         <div className={classes['mainImage']}><img src={mealsImage} alt="Table full of delicious food"/></div>
     </Fragment>);
