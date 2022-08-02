80fdf4a50e8f043f05b7e3553759639d23837d6a
rudgal <office@galler.digital>
2022-08-02 18:48:49 +0200

    Section 21: fix references/imports

--- code/02-finished/src/components/comments/Comments.js
+++ code/02-finished/src/components/comments/Comments.js
@@ -1,12 +1,12 @@
-import { useState, useEffect, useCallback } from 'react';
-import { useParams } from 'react-router-dom';
+import { useCallback, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
-import classes from './Comments.module.css';
-import NewCommentForm from './NewCommentForm';
-import useHttp from '../../hooks/use-http';
-import { getAllComments } from '../../lib/api';
-import LoadingSpinner from '../ui/LoadingSpinner';
-import CommentsList from './CommentsList';
+import classes from "./Comments.module.css";
+import NewCommentForm from "./NewCommentForm";
+import useHttp from "../../hooks/use-http";
+import { getAllComments } from "../../lib/api";
+import CommentsList from "./CommentsList";
+import LoadingSpinner from "../UI/LoadingSpinner";
 
 const Comments = () => {
   const [isAddingComment, setIsAddingComment] = useState(false);
@@ -30,30 +30,30 @@ const Comments = () => {
 
   let comments;
 
-  if (status === 'pending') {
+  if (status === "pending") {
     comments = (
-      <div className='centered'>
+      <div className="centered">
         <LoadingSpinner />
       </div>
     );
   }
 
-  if (status === 'completed' && loadedComments && loadedComments.length > 0) {
+  if (status === "completed" && loadedComments && loadedComments.length > 0) {
     comments = <CommentsList comments={loadedComments} />;
   }
 
   if (
-    status === 'completed' &&
+    status === "completed" &&
     (!loadedComments || loadedComments.length === 0)
   ) {
-    comments = <p className='centered'>No comments were added yet!</p>;
+    comments = <p className="centered">No comments were added yet!</p>;
   }
 
   return (
     <section className={classes.comments}>
       <h2>User Comments</h2>
       {!isAddingComment && (
-        <button className='btn' onClick={startAddCommentHandler}>
+        <button className="btn" onClick={startAddCommentHandler}>
           Add a Comment
         </button>
       )}
